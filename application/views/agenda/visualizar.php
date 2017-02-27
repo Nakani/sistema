43e3a8914072a8fa6a6aba2770b6d04177ce8237
nakani <gripho.poa@gmail.com>
2017-02-27 02:23:59 -0300

    atualizacao view agenda

--- application/views/agenda/visualizar.php
+++ application/views/agenda/visualizar.php
@@ -7,8 +7,16 @@
                                 <div class="row">
                                     <div class="col-md-12">                        
                                         <div class="form-group">
-                                            <H3>Sala <?php echo $reserva->sala ?></H3>
-                                            <H3>Reservado por <?php echo $reserva->nome ?></H3>
+                                            <H3>Sala <strong><?php echo $reserva->sala ?></strong> já Reservada por <?php echo $reserva->nome; ?></H3>
+                                            <?php 
+                                                $data_ini = $reserva->data_ini;
+                                                $dataIni = date('d/m/Y H:m:s', strtotime($data_ini));
+                                                $data_fim = $reserva->data_ini;
+                                                $dataFim = date('d/m/Y H:m:s', strtotime($data_fim));
+                                            ?>
+                                            <h4>dia <?php echo $dataIni?> até <?php echo $dataFim ?></h4>
+
+
                                         </div>                        
                                     </div>
                                 </div>
